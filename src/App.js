1a28cf98d8fccd28c8c20014ca9c573ee581ecc4
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-11-28 20:48:11 +0300

    .

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import './index.css';
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center text-blue-600">
+      <div>Hello</div>
+      <h1>sj</h1>
+    </div>
+  );
+}
+
+export default App;
