4f4e44ba33ec64bd97129b75f9985690a7730466
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-11-29 18:48:12 +0300

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../img/logo.png';
+import { BsBasket } from 'react-icons/bs';
 
 const Header = () => {
   return (
-    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+    <header className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
         {/* DESCTOP & TABLET  */}
-        <div className="hidden md:flex w-full h-full bg-red-600 p-4"></div>
+        <div className="hidden md:flex w-full h-full items-center justify-between">
+          <div className="flex items-center gap-2">
+              <img src={Logo} className="w-8 object-cover" alt="logo" />
+              <p className="text-headingColor text-xl font-bold">Город</p>
+          </div>
+
+          <ul className="flex items-center gap-8">
+              <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</li>
+              <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Menu</li>
+              <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">About us</li>
+              <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Service</li>
+          </ul>
+
+          <div className="relative flex items-center justify-center">
+              <BsBasket className="text-textColor text-2xl ml-8 cursor-pointer" />
+              <div className="absolute -top-2 right-0 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+                  <p className="text-xs text-white font-semibold">2</p>
+              </div>
+          </div>
+        </div>
 
 
         {/* MOBILE  */}
-        <div className="flex md:hidden w-full h-full bg-blue-500 p-4"></div>
-    </div>
+        <div className="flex md:hidden w-full h-full"></div>
+    </header>
   )
 }
 
