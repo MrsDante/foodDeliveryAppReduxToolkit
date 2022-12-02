a0ff2917a58b7e140cbb6d710577d9cfbe17a559
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-12-02 19:20:20 +0300

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,7 +41,7 @@ const Header = () => {
   }
 
   return (
-    <header className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+    <header className="fixed z-50 w-screen bg-slate-300 p-3 px-4 md:p-6 md:px-16">
         {/* DESCTOP & TABLET  */}
         <div className="hidden md:flex w-full h-full items-center justify-between">
           <Link to={'/'} className="flex items-center gap-2">
@@ -102,7 +102,49 @@ const Header = () => {
 
 
         {/* MOBILE  */}
-        <div className="flex md:hidden w-full h-full"></div>
+        <div className="flex items-center md:hidden justify-between  w-full h-full">
+          <Link to={'/'} className="flex items-center gap-2">
+              <img src={Logo} className="w-8 object-cover" alt="logo" />
+              <p className="text-headingColor text-xl font-bold">Город</p>
+          </Link>
+
+          <div className="relative">
+              <motion.img whileTap={{ scale: 0.6}}
+                alt="avatar" 
+                src={user ? user.photoURL : Avatar} 
+                className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-2xl cursor-pointer rounded-full"
+                onClick={handleLogin} 
+                />
+                
+                {
+                    isMenu && (
+                        <motion.div initial={{opacity : 0, scale: 0.6}}
+                                    animate={{opacity : 1, scale: 1}}
+                                    exit={{opacity : 0, scale: 0.6}}
+                                    className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
+                        {
+                            user && user.email === 'sharapova.anastasiiya@gmail.com' && (
+                            <Link to={'/createItem'}>
+                              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transitial-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
+                            </Link>
+                            )}
+
+                            <ul
+                               className="flex flex-col px-4 py-2 gap-8">
+                               <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</li>
+                               <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Menu</li>
+                               <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">About us</li>
+                               <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Service</li>
+                            </ul>
+
+                        <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transitial-all duration-100 ease-in-out text-textColor text-base">Logout <MdLogout /></p>
+                      </motion.div>
+                    )
+                }
+
+
+            </div>
+        </div>
     </header>
   )
 }
